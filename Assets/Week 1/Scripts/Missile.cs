05ccc5a81c119b3d8a389f8ee6ef26bdd06e033d
rennicka <rennicka@sheridancollege.ca>
2024-01-18 10:14:16 -0500

    Task 1 complete: collision are made to the missile

--- Assets/Week 1/Scripts/Missile.cs
+++ Assets/Week 1/Scripts/Missile.cs
@@ -1,19 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
-    float speed = 5f;
+    public Vector2 speed = new Vector2(10, 0);
+    Rigidbody2D rigidbody;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(1 * speed * Time.deltaTime, 0, 0);
+        //transform.Translate(1 * speed * Time.deltaTime, 0, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
     }
 }
