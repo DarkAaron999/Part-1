c15be325e5db9d0d4a532eb3283e168bcdb1281d
rennicka <rennicka@sheridancollege.ca>
2024-01-25 18:23:09 -0500

    Task4b completed: Added oilspill, cones, boost, and used cinemachine to confined the camara

--- Assets/Week 3/Scripts/Car.cs
+++ Assets/Week 3/Scripts/Car.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Car : MonoBehaviour
 {
     float acceleration;
     float steering;
+    public float rotationSpeed = 100;
     public float forwardSpeed = 500;
     public float steeringSpeed = 200;
+    public float boostSpeed = 25000;
     Rigidbody2D rigidbody;
     void Start()
     {
@@ -26,5 +29,22 @@ public class Car : MonoBehaviour
         rigidbody.AddTorque(steering * -steeringSpeed * Time.deltaTime);
         Vector2 force = transform.up * acceleration * forwardSpeed * Time.deltaTime;
         rigidbody.AddForce(force);
+
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Oillspill")
+        {
+            rigidbody.AddTorque(rigidbody.rotation + rotationSpeed * Time.deltaTime);
+        }
+
+        if (collision.gameObject.tag == "Boost")
+        {
+            Debug.Log("Entered");
+
+            Vector2 force = transform.up * acceleration * boostSpeed * Time.deltaTime;
+            rigidbody.AddForce(force);
+        }
     }
 }
