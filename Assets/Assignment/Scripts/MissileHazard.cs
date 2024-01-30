a52a98e9bcb5d11013ad5be6893e0b4ad1c72c19
rennicka <rennicka@sheridancollege.ca>
2024-01-29 20:56:59 -0500

    Task 3 Missile script completed: Made a script for the missile to make it destory it self and the player when collision happens

---
+++ Assets/Assignment/Scripts/MissileHazard.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MissileHazard : MonoBehaviour
+{
+    // For the speed of missile
+    public Vector2 speed = new Vector2 (10, 0);
+    // To use the rigidbody
+    Rigidbody2D rigidbody;
+
+    public GameObject player;
+    void Start()
+    {
+        // To get the component for rigidbody
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Function for movement of missile
+    private void FixedUpdate()
+    {
+        // To make the missile move
+        rigidbody.MovePosition(rigidbody.position +  speed * Time.deltaTime);
+    }
+
+    // Function for on collision enter
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Destory the missile gameobject
+        Destroy(gameObject);
+        // Destory the player gameobject
+        Destroy(player);
+    }
+}
